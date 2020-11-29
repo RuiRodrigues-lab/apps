5aa49ad183be2cfe488efebb8c79472a10c31c71
Rui Rodrigues <73064724+RuiRodrigues-lab@users.noreply.github.com>
2020-11-29 17:16:12 +0000

    Add files via upload

---
+++ spambot1/spambot1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace spambot1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a frase1?");
+            string frase1 = Console.ReadLine();
+
+            Console.WriteLine("Qual a frase2?");
+            string frase2 = Console.ReadLine();
+
+            Console.WriteLine("Qual a frase3?");
+            string frase3 = Console.ReadLine();
+
+            Console.WriteLine("Qual a frase4?");
+            string frase4 = Console.ReadLine();
+
+            string[] frases = { frase1, frase2, frase3, frase4 };
+
+            Console.WriteLine("Qual é o tempo em milisegundos entre frases?");
+            int tempo = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Quantas repeticoes queres?");
+            int repeticoes = Convert.ToInt16(Console.ReadLine());
+
+            Random rng = new Random();
+
+            for (int i = 0; i < repeticoes; i++)
+            {
+                int pos = rng.Next(0, 4);
+                Console.WriteLine(frases[pos]);
+                Thread.Sleep(tempo);
+            }
+            Console.ReadKey();
+        }
+    }
+}
